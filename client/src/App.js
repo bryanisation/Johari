285ba477d5de74003b4bb66694e1591a99e5cd98
Odongo Bryan <bryanisation@gmail.com>
2020-04-03 12:35:11 +0300

    This is the initial staging of Johari after implementing both the server and the client

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (<div className="">
+    <header className="navbar bg-light navbar-light">
+      <h1 className="navbar-brand">
+        <a href="#">Odongo Bryan</a>
+      </h1>
+      <nav className="">
+        <ul className="main-nav">
+          <a className="nav-item nav-link" href="#">
+            About
+          </a>
+
+          <a className="nav-item nav-link" href="#">
+            Portfolio
+          </a>
+          <a className="nav-item nav-link" href="#">
+            Contact Me
+          </a>
+        </ul>
+      </nav>
+    </header>
+  </div>);
+}
+
+export default App;
