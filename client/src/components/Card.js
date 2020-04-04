babb2e9870097f2583a5a39670f5a2a9cf8897e1
Odongo Bryan <bryanisation@gmail.com>
2020-04-04 04:30:42 +0300

    Card to display indivdual items based on bootstraps card classes and some personal designs.

---
+++ client/src/components/Card.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+const Card = () => {
+  <div className="card">
+    <div className="card-header"></div>
+    <div className="card-body"></div>
+    <div className="card-footer"></div>
+  </div>
+}
+
+export default Card;
---
+++ client/src/components/content.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import './Card'
+
+const Container = () => {
+  <div className="container">
+    <card/>
+  </div>
+}
